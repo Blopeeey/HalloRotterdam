e2aa86ac9f0c1bb57977a9dd4000e28505ab512e
blopeeey <basweidmann@hotmail.com>
2021-03-22 14:24:42 +0100

    added new greetings

--- HalloRotterdam/Program.cs
+++ HalloRotterdam/Program.cs
@@ -7,7 +7,8 @@ namespace HalloRotterdam
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Rotterdam!");
-            Console.WriteLine("Yeeha");
+            Console.WriteLine("Yeehaw");
+            Console.WriteLine("Yeehaw2");
         }
     }
 }
